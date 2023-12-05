67bdcc4340a42f1449a9cc4015923944b967f06e
gj19866 <gj19866@bp1-login03.data.bp.acrc.priv>
2023-12-05 18:10:01 +0000

    Job Files running

--- src/BCs/SuperBC2.C
+++ src/BCs/SuperBC2.C
@@ -1,34 +1,35 @@
-#include "SuperBC2.h"
-#include <cmath>
+// #include "SuperBC2.h"
+// #include <cmath>
 
 
-registerMooseObject("SCResApp", SuperBC2);
+// registerMooseObject("SCResApp", SuperBC2);
 
-InputParameters
-SuperBC2::validParams()
-{
-  InputParameters params = ADDirichletBCBase::validParams();
+// InputParameters
+// SuperBC2::validParams()
+// {
+//   InputParameters params = ADDirichletBCBase::validParams();
 
-  // Specify input parameters that we want users to be able to set:
-  params.addParam<Real>("psi_MAG", 0, "Mag of Psi on the boundary");
-  params.addRequiredCoupledVar("Psi_Im", "Value on the boundary");
-  return params;
-}
+//   // Specify input parameters that we want users to be able to set:
+//   params.addParam<Real>("psi_MAG", 0, "Mag of Psi on the boundary");
+//   params.addRequiredCoupledVar("Psi_Im", "Value on the boundary");
+//   return params;
+// }
 
-SuperBC2::SuperBC2(const InputParameters & parameters)
-  : ADDirichletBCBase(parameters),
-    // store the user-specified parameters from the input file...
-    _psi_MAG(getParam<Real>("psi_MAG")),        // for the multiplier
-    _Psi_Im(adCoupledValue("Psi_Im")) // for the coupled variable
-{
-}
+// SuperBC2::SuperBC2(const InputParameters & parameters)
+//   : ADDirichletBCBase(parameters),
+//     // store the user-specified parameters from the input file...
+//     _psi_MAG(getParam<Real>("psi_MAG")),        // for the multiplier
+//     _Psi_Im(adCoupledValue("Psi_Im")) // for the coupled variable
+// {
+// }
 
-ADReal
-SuperBC2::computeQpValue()
-{
-  // For dirichlet BCS, u=BC at the boundary, so the residual includes _u and the desired BC value:
+// ADReal
+// SuperBC2::computeQpValue()
+// {
+//   // For dirichlet BCS, u=BC at the boundary, so the residual includes _u and the desired BC value:
 
-//   return _u[_qp]*_u[_qp] + _Psi_Im[_qp]*_Psi_Im[_qp] - (_psi_MAG);
-return std::sqrt((_psi_MAG) - (_Psi_Im[_qp]*_Psi_Im[_qp]));
+// //   return _u[_qp]*_u[_qp] + _Psi_Im[_qp]*_Psi_Im[_qp] - (_psi_MAG);
+// // return std::sqrt((_psi_MAG) - (_Psi_Im*_Psi_Im));
+// return 0;
 
-}
+// }
--- week1/job.sh
+++ week1/job.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 #SBATCH --job-name=test_job
-#SBATCH --partition=test
+#SBATCH --partition=compute
 #SBATCH --account=phys030845
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --time=0:10:00
-#SBATCH --mem=100M
+#SBATCH --nodes=2
+#SBATCH --ntasks-per-node=24
+#SBATCH --time=10:00:00
+#SBATCH --mem=4000M
 
 module add apps/moose/1.0
+source activate moose
 
-# source activate moose
-
-/user/home/gj19866/projects/sc_res/sc_res-opt -i /user/home/gj19866/projects/sc_res/week1/week10.i
+srun --cpu-bind=cores --mpi=pmi2 /user/home/gj19866/projects/sc_res/sc_res-opt -i /user/home/gj19866/projects/sc_res/week1/week11.i -t
