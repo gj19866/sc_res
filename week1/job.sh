57aab4293d24295c12d421fc8561d9fc09c8b034
gj19866 <gj19866@bp1-login01.data.bp.acrc.priv>
2023-12-07 13:00:40 +0000

    SCS job file

--- week1/job.sh
+++ week1/job.sh
@@ -4,10 +4,10 @@
 #SBATCH --account=phys030845
 #SBATCH --nodes=2
 #SBATCH --ntasks-per-node=24
-#SBATCH --time=10:00:00
+#SBATCH --time=30:00:00
 #SBATCH --mem=4000M
 
 module add apps/moose/1.0
 source activate moose
 
-srun --cpu-bind=cores --mpi=pmi2 /user/home/gj19866/projects/sc_res/sc_res-opt -i /user/home/gj19866/projects/sc_res/week1/week11.i -t
+srun --cpu-bind=cores --mpi=pmi2 /user/home/gj19866/projects/sc_res/sc_res-opt -i /user/home/gj19866/projects/sc_res/week1/week11_2.i -t
