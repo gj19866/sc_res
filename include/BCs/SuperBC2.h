6f299bca5fa28cc330842b9bc0c0dbb82352791a
gj19866 <141067346+gj19866@users.noreply.github.com>
2023-12-29 18:48:09 +0000

    DemoFiles

--- include/BCs/SuperBC2.h
+++ include/BCs/SuperBC2.h
@@ -1,21 +1,21 @@
-#pragma once
+// #pragma once
 
-#include "ADDirichletBCBase.h"
+// #include "ADDirichletBCBase.h"
 
-/// Implements a coupled Dirichlet BC where u = alpha * some_var on the boundary.
-class SuperBC2 : public ADDirichletBCBase
-{
-public:
-  SuperBC2(const InputParameters & parameters);
+// /// Implements a coupled Dirichlet BC where u = alpha * some_var on the boundary.
+// class SuperBC2 : public ADDirichletBCBase
+// {
+// public:
+//   SuperBC2(const InputParameters & parameters);
 
-  static InputParameters validParams();
+//   static InputParameters validParams();
 
-protected:
-  virtual ADReal computeQpValue() override;
+// protected:
+//   virtual ADReal computeQpResidual() override;
 
-private:
-  /// Multiplier on the boundary.
-  Real _psi_MAG;
-  /// reference to a user-specifiable coupled (independent) variable
-  const ADVariableValue & _Psi_Im;
-};
+// private:
+//   /// Multiplier on the boundary.
+//   Real _psi_MAG;
+//   /// reference to a user-specifiable coupled (independent) variable
+//   const ADVariableValue & _Psi_Im;
+// };
--- src/BCs/SuperBCInt.C
+++ src/BCs/SuperBCInt.C
@@ -1,11 +1,11 @@
-#include "SuperBC.h"
+#include "SuperBCInt.h"
 
-registerMooseObject("SCResApp", SuperBC);
+registerMooseObject("SCResApp", SuperBCInt);
 
 InputParameters
-SuperBC::validParams()
+SuperBCInt::validParams()
 {
-  InputParameters params = ADNodalBC::validParams();
+  InputParameters params = ADIntegratedBC::validParams();
 
   // Specify input parameters that we want users to be able to set:
   params.addParam<Real>("psi_MAG", 1, "Mag of Psi on the boundary");
@@ -13,8 +13,8 @@ SuperBC::validParams()
   return params;
 }
 
-SuperBC::SuperBC(const InputParameters & parameters)
-  : ADNodalBC(parameters),
+SuperBCInt::SuperBCInt(const InputParameters & parameters)
+  : ADIntegratedBC(parameters),
     // store the user-specified parameters from the input file...
     _psi_MAG(getParam<Real>("psi_MAG")),        // for the multiplier
     _Psi_Im(adCoupledValue("Psi_Im")) // for the coupled variable
@@ -22,11 +22,10 @@ SuperBC::SuperBC(const InputParameters & parameters)
 }
 
 ADReal
-SuperBC::computeQpResidual()
+SuperBCInt::computeQpResidual()
 {
   // For dirichlet BCS, u=BC at the boundary, so the residual includes _u and the desired BC value:
-  auto u_value = _u;
+//   auto u_value = _u;
 //   return _u[_qp]*_u[_qp] + _Psi_Im[_qp]*_Psi_Im[_qp] - (_psi_MAG);
-return u_value*u_value  + _Psi_Im[_qp]*_Psi_Im[_qp] - (_psi_MAG);
-
+return _test[_i][_qp]*(_u[_qp]*_u[_qp]  + _Psi_Im[_qp]*_Psi_Im[_qp] - (_psi_MAG));
 }
